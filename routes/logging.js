43717a57aed132310241f655446d3ded72f23e20
April Eggers <april.eggers@flatironsjouve.com>
2017-10-10 13:16:58 -0600

    Initial commit of mock logging server

---
+++ routes/logging.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+router.post('/', function (req, res, next) {
+    console.log("The body of the req " + JSON.stringify(req.body));
+    res.sendStatus(200);
+});
+
+module.exports = router;
